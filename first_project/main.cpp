5bf98a4e8edbe68ad83dd838eb891cd5b9d5c1bf
fullStack-Blake <koreanincanada92@gmail.com>
2020-01-08 22:25:57 -0500

    Initial Commit

---
+++ first_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  first_project
+//
+//  Created by 엽쌍 on 2020-01-08.
+//  Copyright © 2020 blake. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
